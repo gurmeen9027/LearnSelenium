26d49bd8978ae06ffd165b4a211bd5d0a452d6b7
Gurmeen_Singh <Gurmeen_Singh@BLRKEC370234D.ad.infosys.com>
2017-05-29 18:32:17 +0530

    all working thumbs up

--- src/test/java/Day9/FD/EMICalc.java
+++ src/test/java/Day9/FD/EMICalc.java
@@ -1,13 +1,18 @@
 package Day9.FD;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import com.relevantcodes.extentreports.ExtentReports;
@@ -16,8 +21,9 @@ import com.relevantcodes.extentreports.LogStatus;
 
 public class EMICalc {
 
-	
-	public static void main(String[] args) {
+	static ChromeDriver driver;
+	static String fpath;
+	public static void main(String[] args) throws IOException {
 		
 		//Reporting
 		ExtentReports eReports = new ExtentReports(System.getProperty("user.dir")+"/Reports/"+ "EMIReport"+ getdtime()+".html");
@@ -25,7 +31,7 @@ public class EMICalc {
 		
 		
 		//Add log
-		Logger EMIlog = Logger.getLogger(EMICalc.class);
+		Logger EMIlog = LogManager.getLogger(EMICalc.class);
 		//DOMConfigurator.configure("log4j.xml");
 		
 		EMIlog.info("Started Execution");
@@ -34,7 +40,7 @@ public class EMICalc {
 		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/src/test/resources/chromedriver.exe");
 		//System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir")+"/src/test/resources/geckodriver.exe");
 
-		ChromeDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		//FirefoxDriver driver = new FirefoxDriver();
 		EMIlog.info("Launch application");
 		
@@ -44,7 +50,7 @@ public class EMICalc {
 		
 		//Finding Web Elements and performing action
 		EMIlog.info("Started Execution");
-		String exp_output  = "1234";
+		String exp_output  = "4614.49";
 		
 		EMIlog.info("Enter the values");
 		driver.findElement(By.id("one")).sendKeys("100000");
@@ -61,14 +67,22 @@ public class EMICalc {
 		if(act_output.equals(exp_output)){
 			System.out.println("Validation is successful");
 			EMIlog.info("Validation successful");
-			startTest.log(LogStatus.PASS, "Validation for execution", "Passed");
+			startTest.log(LogStatus.PASS, "Validation for execution", "Passed" + startTest.addScreenCapture(getScreen()));
 		}else{
 			System.out.println("Validation failed");
 			EMIlog.error("Validation failed");
-			startTest.log(LogStatus.FAIL, "Validation for EMI", "Failed");
+			startTest.log(LogStatus.FAIL, "Validation for EMI", "Failed" + startTest.addScreenCapture(getScreen()));
+
 		}
 		
+		
+		
 
+		
+		
+		//Screenshot
+
+		
 		
 		eReports.endTest(startTest);
 		eReports.flush();
@@ -87,9 +101,21 @@ public class EMICalc {
 	 public static String getdtime(){
 		 Date date = new Date();
 		System.out.println(date);
-		SimpleDateFormat dformat = new SimpleDateFormat("dd-MMM-yyyy hh-mm-ss");
+		SimpleDateFormat dformat = new SimpleDateFormat("dd-MMM-yyyy HH-mm-ss");
 		String dte = dformat.format(date);
+		System.out.println(dte);
 		return dte;
 	 }
 	
+	 
+	 
+	 public static String getScreen() throws IOException{
+		 
+			TakesScreenshot imgcapture=(TakesScreenshot)driver;
+			File screen = imgcapture.getScreenshotAs(OutputType.FILE);
+			fpath= System.getProperty("user.dir")+"/Reports/"+ "EMIReport" + getdtime() + ".png";
+			FileUtils.copyFile(screen, new File(fpath));
+			return fpath;
+			
+	 }
 }
